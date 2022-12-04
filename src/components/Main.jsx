3136d759b71465969f0d7216afd6046af3a558c1
Ajay KC <meajay64@gmail.com>
2022-12-04 23:26:30 +0900

    Add CountryCard component to Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import CountryCard from "./CountryCard";
 import Input from "./Input";
 import Select from "./Select";
+import data from "../../data";
 
 function Main() {
+    
   return (
     <main className="main">
       <div className="flex input">
         <Input />
         <Select />
       </div>
+      <CountryCard data={data} />
     </main>
   );
 }
