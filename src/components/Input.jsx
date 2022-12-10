1eb6d6d65f53dab9cc554b20bdfa132741a2345d
Ajay KC <meajay64@gmail.com>
2022-12-11 02:22:48 +0900

    Add input component logic

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-function Input() {
-  return <input className="main_input" type="text" placeholder="Search for a country" />;
+function Input({ handleChange, name }) {
+  return (
+    <input
+      onChange={handleChange}
+      name="country"
+      className="main_input"
+      type="text"
+      placeholder="Search for a country"
+      value={name}
+    />
+  );
 }
 
 export default Input;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,14 +7,43 @@ import { useState, useEffect } from "react";
 
 function Main() {
   const [data, setData] = useState([]);
+  const [name, setName] = useState("");
+  const [region, setRegion] = useState("");
   const [index, setIndex] = useState(5);
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
+  let [filteredByInputData, setFilteredByInputData] = useState(null);
+  let countrydata;
+
+  const handleChange = (e) => {
+    if (e.target.name === "region") {
+      setRegion(e.target.value);
+      if (name) {
+        let filteredData = data.filter((obj) => {
+          return obj.name.common.toLowerCase() === name.toLowerCase();
+        });
+        setFilteredByInputData(filteredData);
+      }
+      // setName("");
+    } else if (e.target.name === "country") {
+      let filteredData = data.filter((obj) => {
+        return obj.name.common.toLowerCase() === e.target.value.toLowerCase();
+      });
+      setFilteredByInputData(filteredData);
+      setName(e.target.value);
+    }
+    setIndex(5);
+  };
+
   const handleClick = () => {
     setIndex(index + 5);
   };
+
   useEffect(() => {
-    fetch("https://restcountries.com/v3.1/all")
+    let link = region
+      ? `https://restcountries.com/v3.1/region/${region}`
+      : "https://restcountries.com/v3.1/all";
+    fetch(link)
       .then((res) => res.json())
       .then(
         (result) => {
@@ -26,30 +55,36 @@ function Main() {
           setError(error);
         }
       );
-  }, []);
-
+  }, [region]);
+  console.log(filteredByInputData);
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
     return <div>Loading...</div>;
-  } else {
-    let countrydata = data.map((obj, i) => {
+  }
+  if (filteredByInputData) {
+    countrydata = filteredByInputData.map((obj, i) => {
       if (i < index) {
         return <CountryCard key={i} data={obj} />;
       }
     });
-
-    return (
-      <main className="main">
-        <div className="flex input">
-          <Input />
-          <Select />
-        </div>
-        <section className="country-info">{countrydata}</section>
-        <button onClick={handleClick}>See More</button>
-      </main>
-    );
+  } else {
+    countrydata = data.map((obj, i) => {
+      if (i < index) {
+        return <CountryCard key={i} data={obj} />;
+      }
+    });
   }
+  return (
+    <main className="main">
+      <div className="flex input">
+        <Input name={name} handleChange={handleChange} />
+        <Select handleChange={handleChange} />
+      </div>
+      <section className="country-info">{countrydata}</section>
+      <button onClick={handleClick}>See More</button>
+    </main>
+  );
 }
 
 export default Main;
