d8f778d7efd7935de50fca4f83b5bc0c8d588d74
Ajay KC <meajay64@gmail.com>
2022-12-11 16:29:48 +0900

    Add styles for card component

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,17 +1,30 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 function CountryCard({ data }) {
   return (
-    <div className="card">
-      <img src={data.flags.png} />
-      <div className="card-info">
-        <h2>{data.name.common}</h2>
-        <p>Population: {data.population}</p>
-        <p>Region: {data.region}</p>
-        <p>Capital: {data.capital}</p>
-      </div>
-    </div>
+    <Card className="card" style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={data.flags.png} />
+      <Card.Body className="px-2 pt-2 pb-1">
+        <Card.Title className="card-title">{data.name.common}</Card.Title>
+        <Card.Text className="card-text"><strong>Population</strong>: {data.population}</Card.Text>
+        <Card.Text className="card-text"><strong>Region</strong>: {data.region}</Card.Text>
+        <Card.Text className="card-text"><strong>Capital</strong>: {data.capital}</Card.Text>
+      </Card.Body>
+    </Card>
   );
+
+  // return (
+  //   <div className="card">
+  //     <img src={data.flags.png} />
+  //     <div className="card-info">
+  //       <h2>{data.name.common}</h2>
+  //       <p>Population: {data.population}</p>
+  //       <p>Region: {data.region}</p>
+  //       <p>Capital: {data.capital}</p>
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default CountryCard;
