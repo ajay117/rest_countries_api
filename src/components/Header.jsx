1736444ddb142f08a9f24e4136658d4683eb76cf
Ajay KC <meajay64@gmail.com>
2022-12-17 18:55:01 +0900

    Add padding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import MoonIcon from "../assets/nightlight_black_24dp.svg";
 
 function Header() {
   return (
-    <header className="flex py-2">
+    <header className="flex py-2 px-3">
       <h1 className="my-0">Where in the world?</h1>
       <div className="flex">
         <img className="tilt" src={MoonIcon} alt="" />
