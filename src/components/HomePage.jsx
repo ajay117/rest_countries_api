6a814f458b1a3ba8018fe09f00f45569d1c77e47
Ajay KC <meajay64@gmail.com>
2022-12-18 00:54:14 +0900

    Add a component for country details

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+function CountryDetails() {
+  return (
+    <main>
+      <div>
+        <button>Back</button>
+      </div>
+    </main>
+  );
+}
+
+export default CountryDetails;
