74f379b2684dd372580ec91d5e8bf49e02745738
Ajay KC <meajay64@gmail.com>
2022-12-17 23:45:31 +0900

    Add React Router to app

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import "../src/stylesheets/App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CountryDetails from "./components/CountryDetails";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route index element={<Main />} />
+            <Route path="details" element={<CountryDetails />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
