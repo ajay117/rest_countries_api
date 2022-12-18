fe5b6a327f8985edf600a9c3443ffe29e0b9b551
Ajay KC <meajay64@gmail.com>
2022-12-18 16:09:51 +0900

    Add basic styles

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -5,8 +5,8 @@ function CountryDetails({ details }) {
   const obj = details[0];
   // const regionNames = new Intl.DisplayNames(["en"], { type: "language" });
   return (
-    <main>
-      <div>
+    <main className="container fluid">
+      <div className="my-5">
         <button onClick={() => window.location.reload()}>Back</button>
       </div>
 
@@ -14,33 +14,48 @@ function CountryDetails({ details }) {
       <div className="row">
         <img
           src={obj.flags.png}
-          className="img-fluid col-lg-4"
+          className="img-fluid col-lg-4 mb-5"
           alt={`Flag of ${obj.name.common}`}
         />
         <div className="col-lg-8">
           <h1>{obj.name.common}</h1>
-          <div>
+          <div className="d-flex justify-content-between">
             <div>
               <p>
-                Native Name: {Object.values(obj.name.nativeName).pop().common}
+                <b>Native Name:</b>{" "}
+                {Object.values(obj.name.nativeName).pop().common}
               </p>
-              <p>Population: {obj.population}</p>
-              <p>Region: {obj.region}</p>
-              <p>Sub Region: {obj.subregion}</p>
-              <p>Capital: {obj.capital}</p>
+              <p>
+                <b>Population</b>: {obj.population}
+              </p>
+              <p>
+                <b>Region</b>: {obj.region}
+              </p>
+              <p>
+                <b>Sub Region:</b> {obj.subregion}
+              </p>
+              <p>
+                <b>Capital</b>: {obj.capital}
+              </p>
             </div>
             <div>
-              <p>Top Level Domain: {obj.tld}</p>
-              <p>Currencies: {Object.values(obj.currencies).pop().name}</p>
-              <p>Languages: {Object.values(obj.languages).join(" ")}</p>
+              <p>
+                <b>Top Level Domain:</b>
+                {obj.tld}
+              </p>
+              <p>
+                <b>Currencies:</b> {Object.values(obj.currencies).pop().name}
+              </p>
+              <p>
+                <b>Languages:</b> {Object.values(obj.languages).join(",")}
+              </p>
             </div>
           </div>
-          <p>Border Countries: {Object.values(obj.borders).join(" ")}</p>
+          <p>
+            <b>Border Countries:</b> {obj.borders.join(",")}
+          </p>
         </div>
       </div>
-
-      {/* info */}
-      {console.log(details)}
     </main>
   );
 }
