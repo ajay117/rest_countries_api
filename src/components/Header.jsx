754533660550616c702965209b64d56b7d80d4f9
Ajay KC <meajay64@gmail.com>
2022-12-18 23:09:47 +0900

    Handle dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import MoonIcon from "../assets/nightlight_black_24dp.svg";
 
-function Header() {
+function Header({ handleDarkMode }) {
   return (
-    <>
-      <header className="flex py-2 px-3">
+    <header className="py-2 border-bottom border-3">
+      <div className="d-flex justify-content-between container fluid">
         <h1 className="my-0">Where in the world?</h1>
-        <div className="flex">
+        <div className="flex cursor-pointer" onClick={handleDarkMode}>
           <img className="tilt" src={MoonIcon} alt="" />
           <p className="my-0">Dark Mode</p>
         </div>
-      </header>
-      <Outlet />
-    </>
+      </div>
+    </header>
   );
 }
 
