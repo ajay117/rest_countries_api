3b0025026b3e99c14bcd2bc5d57d4af1bb581200
Ajay KC <meajay64@gmail.com>
2023-01-21 00:24:02 +0900

    Add one region and correct spelling

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -10,10 +10,11 @@ function Select({ handleChange, region }) {
     >
       <option value="">Filter by region</option>
       <option value="africa">Africa</option>
-      <option value="america">America</option>
+      <option value="americas">America</option>
       <option value="asia">Asia</option>
       <option value="europe">Europe</option>
       <option value="oceania">Oceania</option>
+      <option value="antarctic">Antarctic</option>
     </select>
   );
 }
